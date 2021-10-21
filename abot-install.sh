aeb836db75f1021f9f2647d5ab8f960d7f44149a
Osip Shor <92769582+osipshor@users.noreply.github.com>
2021-10-21 17:38:00 +0800

    Update abot-install.sh

--- abot-install.sh
+++ abot-install.sh
@@ -7,7 +7,7 @@ if [ -f ${defaultBotName} ]; then
 fi
 
 echo "[1/3] Загружаю последнюю версию бота..."
-wget -q https://drive.google.com/file/d/1e2jWeWq24kkcYHO6JBOiEAyy1PgyTCcu/view?usp=sharing
+wget -q https://github.com/osipshor/spot-a-bot/releases/download/1.483/A-Bot
 echo "[2/3] Бот загружен, выставляю права доступа..."
 chmod 755 ${defaultBotName}
 echo "[3/3] Всё готово, запускайте бота командой: ./${defaultBotName}"
